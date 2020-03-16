48dcf33a642ba90921b982e0c79e8d310db365fa
Federico <federicobartoli@me.com>
2020-03-16 14:17:32 +0100

    svolgimento esercizio..

--- js/main.js
+++ js/main.js
@@ -1,30 +1,24 @@
 $(document).ready(function () {
 
-
-     // var numeroUtente = parseInt(prompt('Inserisci un numero da 0 a 9'));
-
-$.ajax({
-     url: 'https://flynn.boolean.careers/exercises/api/random/int',
-     method: 'GET',
-     success: function(data) {
-          var numeroPC = data.response;
-          console.log(numeroPC);
-          vincitore(numeroUtente, numeroPC);
-     },
-     error: function(){
-          alert('ERRORE')
-     }
+$('.square').click(function(){
+     $.ajax({
+          url: 'https://flynn.boolean.careers/exercises/api/random/int',
+          method: 'GET',
+          success: function(data) {
+               var numeroPC = data.response;
+               console.log(numeroPC);
+          },
+          error: function(){
+               alert('ERRORE')
+          }
+     });
 });
 
 
 
+
+
+
+
+
 });
-
-function vincitore(numUtente , numPC) {
-
-     if(numUtente > numPC) {
-          console.log('hai vinto');
-     } else {
-          console.log('hai perso!');
-     }
-}
